dcf7de9db13132fd3000f7b31887dbca7fbfa6be
rania <raniaarbash@hotmail.com>
2020-11-06 06:19:35 -0500

    Initial Commit

---
+++ Midterm_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Midterm_Test
+//
+//  Created by Rania Arbash on 2020-11-06.
+//  Copyright © 2020 Rania Arbash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
